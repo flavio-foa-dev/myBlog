10755d83941c12c283644c9aed9eefb848035ee9
Flavio Andrade <foa12381@gmail.com>
2023-05-02 10:16:27 -0300

    hero

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,7 +1,19 @@
 import styles from './Hero.module.css'
+import circuloColorido from '../../assets/circulo_colorido.png'
+import minhaFoto from '../../assets/minhafoto.png'
+
 
 export default function Hero() {
   return (
-    <div className={styles.Banner}>Hero</div>
+    <div className={styles.banner}>
+      <div className={styles.apresentacao}>
+       <h1 className={styles.titulo}> My Blog</h1>
+       <p className={styles.paragrafo}>Boas vindas ao meu espaço pessoal! Eu sou Flavio Andrade, instrutor e programador de Front-end Back-end. Aqui compartilho vários conhecimentos, espero que aprenda algo novo :)</p>
+      </div>
+      <div className={styles.imagens}>
+        <img src={circuloColorido} alt="ciculo" className={styles.circuloColorido} aria-hidden={true} />
+        <img src={minhaFoto} alt="foto" className={styles.minhaFoto}/>
+      </div>
+    </div>
   )
 }
